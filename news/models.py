b97028646897e1d6fd7995490acd0d57b7d1bb54
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-02-24 18:58:34 +0600

    first commit

---
+++ news/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class News(models.Model):
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+
+    name = models.CharField(max_length=100, verbose_name='заголовок')
+    image = models.ImageField(verbose_name='изображение', upload_to='news_images/', null=True)
+    description = models.CharField(max_length=250, verbose_name='краткое описание')
+    content = models.TextField(verbose_name='контент')
+    date = models.DateTimeField(verbose_name='дата добавление')
+    is_published = models.BooleanField(default=True, verbose_name='публичность')
+
+    def __str__(self):
+        return f'{self.name} - {self.date}'
+
+# Create your models here.
