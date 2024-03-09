fb7b7a6a58bb69e879fd45faea4a6fbcc89c37f9
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-03-09 19:16:30 +0600

    admin | part 1

--- news/admin.py
+++ news/admin.py
@@ -1,8 +1,52 @@
 from django.contrib import admin
-from .models import News, Category, Tag
+from .models import News, Category, Tag, AdditionalNewsInfo
+from django.utils.safestring import mark_safe
 
-admin.site.register(News)
-admin.site.register(Category)
-admin.site.register(Tag)
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'date', 'is_published', 'get_image')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'description', 'content')
+    list_filter = ('category', 'date', 'is_published', 'tags')
+    readonly_fields = ('date', 'get_full_image')
+    # filter_vertical = ('tags',)
+    filter_horizontal = ('tags',)
+    # raw_id_fields = ('category',)
+
+    @admin.display(description='Изображение')
+    def get_image(self, news: News):
+        return mark_safe(f'<img src="{news.image.url}" width="150px">')
+
+    @admin.display(description='Изображение')
+    def get_full_image(self, news: News):
+        return mark_safe(f'<img src="{news.image.url}" width="50%">')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(AdditionalNewsInfo)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'news_name', 'email', 'rating')
+    list_display_links = ('id', 'news_name')
+    search_fields = ('news__name', 'news__description', 'news__content')
+    list_filter = ('news', 'news__category', 'news__date', 'news__tags')
+    readonly_fields = ('news_name',)
+
+    @admin.display(description='Название')
+    def news_name(self, info: AdditionalNewsInfo):
+        return f'{info.news.name}'
 
 # Register your models here.
--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 
 
 class News(models.Model):
@@ -11,7 +12,7 @@ class News(models.Model):
     image = models.ImageField(verbose_name='изображение', upload_to='news_images/', null=True)
     description = models.CharField(max_length=250, verbose_name='краткое описание')
     content = models.TextField(verbose_name='контент')
-    date = models.DateTimeField(verbose_name='дата добавление')
+    date = models.DateTimeField(verbose_name='дата добавление', auto_now_add=True)
     is_published = models.BooleanField(default=True, verbose_name='публичность')
     category = models.ForeignKey('news.Category', on_delete=models.PROTECT, related_name='news', verbose_name='категория')
     tags = models.ManyToManyField('news.Tag', related_name='news', verbose_name='теги', blank=True)
@@ -30,6 +31,23 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class AdditionalNewsInfo(models.Model):
+
+    class Meta:
+        verbose_name = 'допалнительная информация новости'
+        verbose_name_plural = 'допалнительная информация новостей'
+
+    news = models.OneToOneField('news.News', on_delete=models.CASCADE, related_name='info', verbose_name='новость')
+    link = models.URLField(verbose_name='ссылка')
+    # email = models.CharField(validators=[RegexValidator(r'^[\w\.-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$', message='Введите корректную эл. почту.')]) # models.EmailField()
+    # json_field = models.JSONField()
+    email = models.EmailField(verbose_name='эл. почта')
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], verbose_name='оценка')
+
+    def __str__(self):
+        return f'{self.news}'
 
 
 class Tag(models.Model):
